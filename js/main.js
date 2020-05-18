1af246f00adbed2b05ceabdfeee77ba3e823583b
Paulo Martins <paulofelipe0896@gmail.com>
2020-05-17 21:00:40 -0300

    Introducao ao javascript

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var nome = "Paulo Martins";
+var idade = 23;
+var idade2 = 10;
+var frase = "Brasil já teve uma boa seleção";
+
+// alert(`${nome} tem ${idade} anos.`);
+// alert(`${idade + idade2}`)
+console.log(frase.replace("Brasil", "Brazucas"));
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());
